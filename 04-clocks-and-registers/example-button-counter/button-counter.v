d3226a81b8f9af11ec21d09f82e0eee4de1bfaba
Shawn Hymel <hymelsr@vt.edu>
2021-10-13 20:56:20 -0500

    Added part 4 examples

---
+++ 04-clocks-and-registers/example-button-counter/button-counter.v
@@ -0,0 +1,29 @@
+// Count up on each button press and display on LEDs
+module button_counter (
+
+    // Inputs
+    input       [1:0]   pmod,
+    
+    // Outputs
+    output  reg [3:0]   led
+);
+
+    wire rst;
+    wire clk;
+
+    // Reset is the inverse of the first button
+    assign rst = ~pmod[0];
+
+    // Clock signal is the inverse of second button
+    assign clk = ~pmod[1];
+
+    // Count up on clock rising edge or reset on button push
+    always @ (posedge clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            led <= 4'b0;
+        end else begin
+            led <= led + 1'b1;
+        end
+    end
+    
+endmodule
---
+++ 04-clocks-and-registers/solution-clock-counter/clock-counter.v
@@ -0,0 +1,41 @@
+// Count up each second
+module clock_counter (
+
+    // Inputs
+    input               clk,
+    input               rst_btn,
+    
+    // Outputs
+    output  reg [3:0]   led
+);
+
+    wire rst;
+    reg div_clk;
+    reg [31:0] count;
+    localparam [31:0] max_count = 6000000;
+
+    // Reset is the inverse of the reset button
+    assign rst = ~rst_btn;
+
+    // Count up on (divided) clock rising edge or reset on button push
+    always @ (posedge div_clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            led <= 4'b0;
+        end else begin
+            led <= led + 1'b1;
+        end
+    end
+    
+    // Clock divider
+    always @ (posedge clk or posedge rst) begin
+        if (rst == 1'b1) begin
+            count <= 32'b0;
+        end else if (count == max_count) begin
+            count <= 32'b0;
+            div_clk <= ~div_clk;
+        end else begin
+            count <= count + 1;
+        end
+    end
+    
+endmodule
