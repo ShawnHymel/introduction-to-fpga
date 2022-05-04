50068f0b6d4ae4647853ccfe9ccaf201a8ac1b0d
Daniel Bovensiepen Li <oss@bovi.li>
2022-05-04 23:43:19 +0800

    clock rate at 1hz instead of 0.5hz
    based on the introduction video (https://youtu.be/LwQsyeuf9Sk?t=1052) the counter should increase once per second.

--- 04-clocks-and-procedural-assignments/solution-clock-counter/clock-counter.v
+++ 04-clocks-and-procedural-assignments/solution-clock-counter/clock-counter.v
@@ -27,7 +27,9 @@ module clock_counter (
     wire rst;
     reg div_clk;
     reg [31:0] count;
-    localparam [31:0] max_count = 6000000 - 1;
+    localparam [31:0] max_count = 12000000 - 1;
+    
+    
 
     // Reset is the inverse of the reset button
     assign rst = ~rst_btn;
